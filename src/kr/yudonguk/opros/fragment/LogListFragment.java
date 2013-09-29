0ad074584b6b1015dd240d09b4b7b1391a0cd20d
yudonguk <yudonguk@naver.com>
2013-09-30 07:47:20 +0900

    Logs와 Packages 의 임시 페이지 작성
    - Logs와 Packages 페이지의 Fragment인 LogListFragment와 PackageListFragment 및 각
    Fragment의 레이아웃 xml 작성
    - 작성된 LogListFragment와 PackageListFragment가 화면에 나타나도록
    main_section_fragments 수정
    - 안드로이드 기본 페이저 예제의 잔제인 fragment_main_dummy.xml 제거

---
+++ src/kr/yudonguk/opros/fragment/LogListFragment.java
@@ -0,0 +1,18 @@
+package kr.yudonguk.opros.fragment;
+
+import kr.yudonguk.opros.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LogListFragment extends Fragment
+{
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
+	{
+		return inflater.inflate(R.layout.fragment_log_list, container, false);
+	}
+}
